37f605068087eed93696b776d2166cc8e5fbf544
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-04-03 20:01:45 +0430

    SearchFilter

--- DRF_project/blog/api/views.py
+++ DRF_project/blog/api/views.py
@@ -6,25 +6,32 @@ from rest_framework.generics import (
    DestroyAPIView,
    CreateAPIView
 )
-
+from rest_framework.filters import (
+   SearchFilter,
+   OrderingFilter
+)
 from blog import models
 from .serializers import PostSerializer, PostDetailSerializer, PostCreateUpdateSerializer
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .permissions import IsOwnerOrReadOnly
 
 class PostListAPIView(ListAPIView):
    queryset = models.Post.objects.all()
    serializer_class = PostSerializer
 
+   filter_backends = [SearchFilter]
+   search_fields = ['title', 'des']
+   #http://127.0.0.1:8000/api/posts/?search=masht&ordering=title    -reverse order
 
 class PostDetailAPIView(RetrieveAPIView):
    queryset = models.Post.objects.all()
    serializer_class = PostDetailSerializer
 
 
-
 class PostUpdateAPIView(UpdateAPIView):
    queryset = models.Post.objects.all()
    serializer_class = PostCreateUpdateSerializer
+   permission_classes = [IsOwnerOrReadOnly,IsAuthenticated]
 
    def perform_update(self , serializer):
       serializer.save(user=self.request.user)
@@ -37,6 +44,7 @@ class PostDeleteAPIView(DestroyAPIView):
 class PostCreateAPIView(CreateAPIView):
    queryset = models.Post.objects.all()
    serializer_class = PostCreateUpdateSerializer
+   permission_classes = [IsAuthenticated ,IsAdminUser]
 
    def perform_create(self, serializer):
       serializer.save(user=self.request.user)
